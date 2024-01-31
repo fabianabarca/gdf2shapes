cff1df484b008cfccac96fcd7713335fdd08f185
Fabián Abarca <ensinergia@gmail.com>
2024-01-31 13:46:57 -0300

    Colocar funciones esqueleto del paquete

--- geo2shapes/geo2shapes.py
+++ geo2shapes/geo2shapes.py
@@ -1,2 +1,39 @@
-def suma(a, b):
-    return a + b
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Point
+
+
+def convert(a, b):
+    """Converts a GeoJSON file containing LineString features to a CSV with shape information.
+
+    Parameters
+    ----------
+    a : str
+        (¿Qué entra aquí: WKT, archivo, .geojson?)
+
+    Returns
+    -------
+    b : str
+        Path to the output CSV file.
+
+
+    """
+    return "hola"
+
+
+def validate(a):
+    """Validates a GeoJSON file containing LineString features.
+
+    Parameters
+    ----------
+    a : str
+        (¿Qué entra aquí: WKT, archivo, .geojson?)
+
+    Returns
+    -------
+    b : str
+        Path to the output CSV file.
+
+
+    """
+    return "adiós"
